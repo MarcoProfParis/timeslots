849596763cf64cdf8c1c0d26ad0acda145358201
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-03-28 05:59:58 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,23 +1,3 @@
-function splitArrayIntoConsecutive(arr) {
-    var result = [];
-    var currentChunk = [];
-    
-    for (var i = 0; i < arr.length; i++) {
-        if (i === 0 || arr[i] === arr[i - 1] + 1) {
-            currentChunk.push(arr[i]);
-        } else {
-            result.push(currentChunk);
-            currentChunk = [arr[i]];
-        }
-    }
-    
-    if (currentChunk.length > 0) {
-        result.push(currentChunk);
-    }
-    
-    return result;
-}
-
 function removeBookingsFromAvailableSlots(data) {
   for (const date in data) {
     const availableSlots = data[date].available_slots;
@@ -48,13 +28,13 @@ function removeBookingsFromAvailableSlots(data) {
     data[date].available_slots = consecutiveArrays;
   }
   
-  return JSON.stringify(newData);
+  return data;
 }
 
 window.function = async function(json) {
     if (json.value === undefined) return "Enter your json";
     let jsonData = JSON.parse(json.value);
 
-    const result = removeBookingsFromAvailableSlots(jsonData);
-    return result;
+    const newData = removeBookingsFromAvailableSlots(jsonData);
+    return JSON.stringify(newData);
 }

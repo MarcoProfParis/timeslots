a52b6dcf04707ea3044a402ca3fd538d51cbeced
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-03-28 01:58:59 +0100

    Update function.js

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ window.function = async function(json) {
   if (json.value === undefined) return "Enter your json";
   console.log(`No schedule found for date:`);
   console.log(json);
-  let jsonData = JSON.parse(json);
+  let jsonData = json;
 console.log(jsonData);
   const allAvailableTimeSlots = {};
 

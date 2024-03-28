741a01fac46ed4350d12086ccbe3af294ef9d0ba
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-03-28 02:00:33 +0100

    Update function.js

--- function.js
+++ function.js
@@ -3,7 +3,31 @@ window.function = async function(json) {
   if (json.value === undefined) return "Enter your json";
   console.log(`No schedule found for date:`);
   console.log(json);
-  let jsonData = json;
+  const jsonData = {
+  "schedule": {
+    "2024-03-28": {
+      "available_slots": [
+        {"start_time": "10:00", "end_time": "14:00"},
+        {"start_time": "16:00", "end_time": "17:30"},
+        {"start_time": "18:00", "end_time": "19:00"}
+      ],
+      "bookings": [
+        {"start_time": "10:00", "end_time": "11:00", "booking_id": "BK123", "customer_name": "John Doe", "contact_number": "+1234567890", "email": "john.doe@example.com"},
+        {"start_time": "11:00", "end_time": "12:00", "booking_id": "BK124", "customer_name": "Alice Smith", "contact_number": "+0987654321", "email": "alice.smith@example.com"},
+        {"start_time": "16:30", "end_time": "17:30", "booking_id": "BK125", "customer_name": "marc hugh", "contact_number": "+098712321", "email": "marc.hugh@example.com"}
+      ]
+    },
+    "2024-03-29": {
+      "available_slots": [
+        {"start_time": "09:00", "end_time": "12:00"},
+        {"start_time": "13:00", "end_time": "19:00"}
+      ],
+      "bookings": [
+        {"start_time": "09:30", "end_time": "10:30", "booking_id": "BK125", "customer_name": "Emma Brown", "contact_number": "+1122334455", "email": "emma.brown@example.com"}
+      ]
+    }
+  }
+};
 console.log(jsonData);
   const allAvailableTimeSlots = {};
 

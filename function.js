dfb3f0950336db0ff75764599c26198e27dcb613
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2024-03-28 05:20:56 +0100

    Update function.js

--- function.js
+++ function.js
@@ -8,16 +8,17 @@ function convertToTimeSlot(slot) {
 function removeBookingsFromAvailableSlots(data) {
   const newData = {};
   for (const date in data) {
-    if (data.hasOwnProperty(date)) { // Ensure the property is not inherited
+    if (data.hasOwnProperty(date)) {
       const availableSlots = data[date].available_slots;
       const bookings = data[date].bookings;
       
       if (!Array.isArray(availableSlots) || !Array.isArray(bookings)) {
         console.error("Invalid data format. availableSlots and bookings must be arrays.");
-        continue; // Move to the next date entry
+        continue;
       }
       
-      // Filter out bookings from available slots
+      console.log(`Processing date: ${date}`);
+      
       const continuousSlots = [];
       let currentSlot = null;
       for (const slot of availableSlots) {
@@ -36,13 +37,16 @@ function removeBookingsFromAvailableSlots(data) {
           continuousSlots.push(currentSlot);
       }
       
-      // Convert slots to time intervals
+      console.log(`Continuous Slots for ${date}:`, continuousSlots);
+      
       newData[date] = { "new_available_slots": continuousSlots.map(slot => convertToTimeSlot(slot[0])) };
     }
   }
   
+  console.log("Processed data:", newData);
   return newData;
 }
+
 
 
 window.function = async function(json) {
